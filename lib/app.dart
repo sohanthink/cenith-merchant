b5445221a408c771efa2a274ebcdd92d087d5ccd
Maynul Islam Jibon <maynulislamjibon4@gmail.com>
2025-08-25 21:09:23 +0600

    screen util set up for responsive ui

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:cenith_storage/core/controller_bindings/controller_bindings.dart
 import 'package:cenith_storage/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/routes/app_routes.dart';
 class CenithStorage extends StatelessWidget {
@@ -11,14 +12,19 @@ class CenithStorage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      initialBinding: ControllerBindings(),
-      onGenerateRoute: AppRoutes.routes,
-      theme: AppTheme.themeData
-
-
+    return ScreenUtilInit(
+      designSize:  Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_,child){
+        return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/',
+            initialBinding: ControllerBindings(),
+            onGenerateRoute: AppRoutes.routes,
+            theme: AppTheme.themeData
+        );
+      },
 
     );
   }
