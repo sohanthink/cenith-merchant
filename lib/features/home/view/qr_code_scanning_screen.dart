b2d57845687f79137eac1adfcab15f9c4d18c3c9
Mizanur Rahman <mizanshaheb644@gmail.com>
2025-08-25 22:00:35 +0600

    qr code screen initial

---
+++ lib/features/home/view/qr_code_scanning_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QrCodeScanningScreen extends StatefulWidget {
+  const QrCodeScanningScreen({super.key});
+
+  @override
+  State<QrCodeScanningScreen> createState() => _QrCodeScanningScreenState();
+}
+
+class _QrCodeScanningScreenState extends State<QrCodeScanningScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
