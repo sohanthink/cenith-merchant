b7d805dc46e44c147d51f2d1e7acffd364298b64
maynul-soft <maynulislamjibon4@gmail.com>
2025-09-08 22:19:03 +0600

    complete ui of category card and list

--- lib/features/booking/view/upcoming_view.dart
+++ lib/features/booking/view/upcoming_view.dart
@@ -1,6 +1,9 @@
 import 'package:cenith_marchent/core/constants/app_colors.dart';
-import 'package:cenith_marchent/features/booking/widgets/no_bookings_yet_design.dart';
+import 'package:cenith_marchent/features/common/contact_support_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../widgets/booking_card.dart';
 
 class UpcomingView extends StatefulWidget {
   const UpcomingView({super.key});
@@ -10,11 +13,41 @@ class UpcomingView extends StatefulWidget {
 }
 
 class _UpcomingViewState extends State<UpcomingView> {
+  //TODO: will remove this variable when integrate api
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.scaffoldColor,
-      body: NoBookingsYetDesign(style: Theme.of(context).textTheme));
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 15.h),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 10,
+              itemBuilder: ((context, index) {
+                return BookingCard(
+                  title: 'Mario Chacón Bernal',
+                  date: 'Feb 16, 14:00 - Feb 16, 19:00',
+                  id: 'K571PIZ8',
+                  bags: 2,
+                  status: 'Pending check in',
+                );
+              }),
+            ),
+            ContactSupportText.supportText(context, (){}),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){}, child: Text('Check in/out')),
+            SizedBox(height: 20,)
 
+          ],
+        ),
+      ),
+
+      //TODO: Set condition for if booking is not available when fetch api
+      // NoBookingsYetDesign(style: Theme.of(context).textTheme)
+    );
   }
 }
