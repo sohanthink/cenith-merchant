adaef855b47ab7bb00b67f36101230ccb2a13a4f
maynul-soft <maynulislamjibon4@gmail.com>
2026-04-15 22:18:58 +0600

    check

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'app.dart';
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
 
